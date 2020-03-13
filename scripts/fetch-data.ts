2ff07c63bf1353c141a4b682cb32a740e0c31d19
Shinsuke Hamasho <blab11hamasho@gmail.com>
2020-03-14 02:49:01 +0900

    fetch-data: add github readme fetch function

--- scripts/fetch-data.ts
+++ scripts/fetch-data.ts
@@ -1,26 +1,28 @@
 import * as path from 'path';
+import { fetch } from './fetch';
 
-const ROOT_AWESOME_URL = 'https://raw.githubusercontent.com/sindresorhus/awesome/master/readme.md';
+const ROOT_AWESOME_URL = 'https://github.com/sindresorhus/awesome';
 const CACHE_DIR = path.join(__dirname, 'cache');
 
 const getReadmeUrl = async (gitRepoUrl) => {
-    const repo = 'some/thing';
+    const repo = gitRepoUrl.replace('https://github.com/', '');
     return `https://raw.githubusercontent.com/${repo}/master/readme.md`;
 };
 
 const fetchUrls = async (url) => {
-    return 'url';
+    return await fetch(url);
 };
 
 const fetchAwesomeUrls = async (url) => {
-    return ['test'];
+    const readme = await fetch(url);
+    return [readme];
 };
 
 const main = async () => {
     const rootReadmeUrl = await getReadmeUrl(ROOT_AWESOME_URL);
     const awesomeURLs = await fetchAwesomeUrls(rootReadmeUrl);
     for (let url of awesomeURLs) {
-        console.log(url);
+        //console.log(url);
     }
 
 }
--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,5 +1,54 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as util from 'util';
 import axios from 'axios';
 
-export const fetch = async url: string => {
+const fsReadFile = util.promisify(fs.readFile);
+const fsWriteFile = util.promisify(fs.writeFile);
+const fsExists = util.promisify(fs.exists);
+const fsMkdir = util.promisify(fs.mkdir);
 
+class StorageCache {
+    cacheDir: string;
+
+    constructor() {
+        this.cacheDir = path.join(__dirname, 'cache');
+    }
+
+    async get(url: string): Promise<string> {
+        const fPath = this.urlToPath(url);
+        const exists = await fsExists(fPath);
+        if (exists) {
+            console.log(`cache: HIT: ${url}`);
+            return await fsReadFile(fPath, 'utf8');
+        }
+        console.log(`cache: MISS: ${url}`);
+        return null;
+    }
+
+    async set(url: string, data: string) {
+        console.log(`cache: SAVE: ${url}`);
+        const fPath = this.urlToPath(url);
+        await fsMkdir(this.cacheDir, {recursive: true});
+        await fsWriteFile(fPath, data);
+    }
+
+    urlToPath(url) {
+        const filename = url.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+        return path.join(this.cacheDir, filename);
+    }
+}
+
+const CACHE = new StorageCache();
+
+const fetch = async (url) => {
+    let result = await CACHE.get(url);
+    if (result) {
+        return result;
+    }
+    const resp = await axios.get<string>(url);
+    await CACHE.set(url, resp.data);
+    return resp;
 };
+
+export { fetch };
