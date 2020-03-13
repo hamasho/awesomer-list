d93c76c130db68675289aeee29f86442089c18bb
Shinsuke Hamasho <blab11hamasho@gmail.com>
2020-03-14 02:06:41 +0900

    fetch-data: add mocha/chai template

---
+++ scripts/test/fetch.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('fetch', () => {
+    describe('fetch', () => {
+        it('should run', () => {
+            expect(true).to.equal(false);
+        });
+    });
+});
